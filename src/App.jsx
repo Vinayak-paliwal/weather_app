da46738932d885ef39c4f166dff8179d10d6634c
Vinayak-paliwal <vinayakpaliwal866@gmail.com>
2025-04-30 13:27:59 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import search from './assets/icons/search.svg'
 import { useStateContext } from './Context'
-import { BgLayout, WeatherCard, MiniCard } from './Components'
+import { BgLayout, WeatherCard, MiniCard } from './components'
 
 function App() {
 
